71b35a305ea086f4fcf863afe8cfb8066f47c249
Milk-Cool <43724263+Milk-Cool@users.noreply.github.com>
2023-02-07 13:19:48 +0300

    new api version (?)

--- fap.js
+++ fap.js
@@ -1,5 +1,5 @@
 const buildFap = app => new Promise((resolve, reject) => {
-	fetch(`https://corsanywhere-taloud.onrender.com/https://flipc.org/api/v1/${app.path}?branch=${app.branch}&nowerr=1`, {
+	fetch(`https://corsanywhere-taloud.onrender.com/https://flipc.org/api/v2/${app.path}?branch=${app.branch}&nowerr=1`, {
 		"headers": {
 			"accept": "application/json"
 		},
@@ -7,7 +7,7 @@ const buildFap = app => new Promise((resolve, reject) => {
 	}).then(res => res.json()).then(res => resolve(res.fap.filename)).catch(reject);
 });
 const getFap = app => new Promise((resolve, reject) => {
-	fetch(`https://corsanywhere-taloud.onrender.com/https://flipc.org/api/v1/${app.path}/elf?branch=${app.branch}&nowerr=1`, {
+	fetch(`https://corsanywhere-taloud.onrender.com/https://flipc.org/api/v2/${app.path}/elf?branch=${app.branch}&nowerr=1`, {
 		"headers": {
 			"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
 		},
